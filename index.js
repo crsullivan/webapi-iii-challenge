55c4b40d631bc1c418dbe865edf34e0a733b5ce4
Connor R. Sullivan <crsullivan@protonmail.com>
2019-10-30 14:58:37 -0700

    MVP

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const postRouter = require('./posts/postRouter');
 
 const server = express();
 
+
+function logger(req, res, next) {
+    console.dir([req.originalUrl, req.method, new Date().toISOString()])  
+    next();
+}
+
+server.use(logger)
+
 server.use(express.json());
+
+server.use('/posts', postRouter)
+server.use('/users', userRouter)
 
 server.get('/',(req, res) => {
     console.log("Lambda Project")
@@ -13,9 +24,6 @@ server.get('/',(req, res) => {
         <h1>Lambda Project, web api 3<h1>
     `);
 });
-
-server.use('/posts', postRouter)
-server.use('/users', userRouter)
 
 const port = 8000;
 server.listen(port, () => console.log('\n=== API on port 8000 ===\n'));
--- users/userRouter.js
+++ users/userRouter.js
@@ -4,10 +4,12 @@ const Posts = require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    if (!Object.keys(req.body).includes("name")){
-        return res.status(400).json({ errorMessage: "Please provide a name for the user." })
-    }
+// router.use(validateUserId);
+
+router.post('/', validateUser, (req, res) => {
+    // if (!Object.keys(req.body).includes("name")){
+    //     return res.status(400).json({ errorMessage: "Please provide a name for the user." })
+    // }
     Users.insert(req.body)
         .then(user => {
             res.status(201).json(user);
@@ -18,20 +20,20 @@ router.post('/', (req, res) => {
         });
 });
 
-router.post('/:id/posts', (req, res) => {
-    if (!Object.keys(req.body).includes("text")){
-        return res.status(400).json({ errorMessage: "Please provide text for the post." })
-    }
+router.post('/:id/posts', [validateUserId, validatePost], (req, res) => {
+    // if (!Object.keys(req.body).includes("text")){
+    //     return res.status(400).json({ errorMessage: "Please provide text for the post." })
+    // }
     const post = {...req.body, user_id: req.params.id}; 
     console.log(req.params.id)
     console.log(post)
     Posts.insert(post)
         .then(user => {
-            if (user) {
+            // if (user) {
             res.status(201).json(post);
-            } else {
-                res.status(404).json({ message: "The post with the specified ID does not exist."})
-        }
+        //     } else {
+        //         res.status(404).json({ message: "The post with the specified ID does not exist."})
+        // }
         })
         .catch(error => {
             console.log(error);
@@ -54,15 +56,15 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',  validateUserId, (req, res) => {
     console.log(req.params.id)
     Users.getById(req.params.id) 
     .then(user => {
-        if (user) {
+        // if (user) {
             res.status(200).json(user);
-        } else {
-            res.status(404).json({ message: "The post with the specified ID does not exist."})
-        }
+        // } else {
+        //     res.status(404).json({ message: "The post with the specified ID does not exist."})
+        // }
     })
     .catch(error => {
         console.log(error);
@@ -70,14 +72,14 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts',  validateUserId, (req, res) => {
     Users.getUserPosts(req.params.id) 
         .then(user => {
-            if (user) {
+            // if (user) {
                 res.status(200).json(user);
-            } else {
-                res.status(404).json({ message: "The post with the specified ID does not exist."})
-            }
+        //     } else {
+        //         res.status(404).json({ message: "The post with the specified ID does not exist."})
+        //     }
         })
         .catch(error => {
             console.log(error);
@@ -85,15 +87,15 @@ router.get('/:id/posts', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',  validateUserId, (req, res) => {
     console.log(req.params.id)
     Users.remove(req.params.id)
     .then(user => {
-        if (user) {
+        // if (user) {
         res.status(200).json({ message: "User deleted"});
-        } else {
-            res.status(404).json({ message: "The user with the specified ID does not exist."})
-    }
+    //     } else {
+    //         res.status(404).json({ message: "The user with the specified ID does not exist."})
+    // }
     })
     .catch(error => {
         console.log(error);
@@ -101,18 +103,18 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
-    if (!Object.keys(req.body).includes("name")){
-        return res.status(400).json({ errorMessage: "Please provide a name for the user." })
-    }
+router.put('/:id', [validateUser, validateUserId], (req, res) => {
+    // if (!Object.keys(req.body).includes("name")){
+    //     return res.status(400).json({ errorMessage: "Please provide a name for the user." })
+    // }
     const newInfo = req.body;
     Users.update(req.params.id, newInfo)
       .then(user => {
-        if (user) {
+        // if (user) {
           res.status(200).json(user);
-        } else {
-          res.status(404).json({ message: "The user with the specified ID does not exist." });
-        }
+        // } else {
+        //   res.status(404).json({ message: "The user with the specified ID does not exist." });
+        // }
       })
       .catch(error => {
         // log error to database
@@ -123,18 +125,60 @@ router.put('/:id', (req, res) => {
       });
 });
 
-//custom middleware
+// custom middleware
 
 function validateUserId(req, res, next) {
-
+    const { id } = req.params;
+    console.log(req.params)
+    Users.getById(id)
+      .then(user => {
+        if (user) {
+          next();
+        } else {
+          res.status(400).json({ message: "invalid user id." });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          message: "Error processing request."
+        });
+      });
 };
 
 function validateUser(req, res, next) {
-
+    const { name } = req.params;
+    console.log(req.body)
+    Users.get(name)
+        .then(user => {
+            if (req.body) {
+                if (!req.body.name) {
+                    res.status(400).json({ message: "missing required name field" })
+                } else {
+                    next();
+                }
+            
+            } else {
+                res.status(400).json({ message: "missing user data" })
+            }
+            })
 };
 
 function validatePost(req, res, next) {
-
+    const post = {...req.body, user_id: req.params.id}; 
+    console.log(req.body)
+    console.log(post)
+    Posts.insert(post)
+        .then(user => {
+            if (!req.body.text) {
+                res.status(400).json({ message: "missing required text field" })
+               
+            } if (!req.body.text && !req.body.user_id) {
+                res.status(400).json({ message: "missing post data" })
+            } else {
+                next()
+            }
+        })
 };
 
 module.exports = router;
